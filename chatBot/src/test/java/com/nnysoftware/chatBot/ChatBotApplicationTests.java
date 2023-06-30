36d3a93fde1f49bb71e5c0daa2d7cf538894b7bc
Haoran Zhang <haoranzhangnyu@gmail.com>
2023-06-30 14:05:37 -0400

    maven project added

---
+++ chatBot/src/test/java/com/nnysoftware/chatBot/ChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnysoftware.chatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
